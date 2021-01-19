1ea24ea6ad4d81c8d24d691573ed7d90b8b306d4
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2021-01-19 03:35:40 -0800

    Added several features
    hud toggle
    add all clothes
    add all thoughts

--- Disco Elysium Mod/Disco Elysium Mod/Main.cs
+++ Disco Elysium Mod/Disco Elysium Mod/Main.cs
@@ -2,12 +2,13 @@
 using UnityEngine;
 using UnityModManagerNet;
 using HarmonyLib;
+using PixelCrushers.DialogueSystem;
 
 namespace Disco_Elysium_Mod
 {
     static class RunSpeed
     {
-        public static float runSpeed = 2f;
+        public static float runSpeed = 1f;
     }
 
     static class ClothesChange
@@ -16,13 +17,23 @@ namespace Disco_Elysium_Mod
         public static bool on = true;
     }
 
+
     static class Main
     {
         public static bool enabled;
         public static UnityModManager.ModEntry mod;
+
+        public static ThoughtsAndItemsTests tester = new ThoughtsAndItemsTests();
+
         public static string speed;
         public static string money;
-        public static bool fixClothes;
+        public static string skillPoints;
+
+        public static bool allThoughtsAdded = false;
+        public static bool allClothesAdded = false;
+
+        public static bool hudVisible = true;
+        public static bool hudIsOn = true;
 
 
         static bool Load(UnityModManager.ModEntry modEntry)
@@ -34,8 +45,6 @@ namespace Disco_Elysium_Mod
             modEntry.OnToggle = OnToggle;
             modEntry.OnUpdate = OnUpdate;
             modEntry.OnGUI = OnGUI;
-
-            RunSpeed.runSpeed = 2f;
 
             return true;
         }
@@ -53,23 +62,14 @@ namespace Disco_Elysium_Mod
             if (Input.GetKeyDown(KeyCode.B))
             {
                 modEntry.Logger.Log("B was pressed!");
-                ClothesChange.on = !ClothesChange.on;
             }
         }
 
         static void OnGUI(UnityModManager.ModEntry modEntry)
         {
-            GUILayout.Label("RUN SPEED MULTIPLIER\n[1.0 - 3.0]");
+            GUILayout.Label("Run Speed Multiplier\n[1.0 - 3.0]");
             speed = GUILayout.TextField(speed, GUILayout.Width(100f));
-
-            GUILayout.Label("Money\n[0 - 99]");
-            money = GUILayout.TextField(money, GUILayout.Width(100f));
-
-            GUILayout.Label("Changing Clothes Doesn't Change Appearance");
-            fixClothes = GUILayout.Toggle(fixClothes,GUIContent.none,GUILayout.Width(40f));
-
-
-            if (GUILayout.Button("Apply"))
+            if (GUILayout.Button("Apply", GUILayout.Width(100f)))
             {
                 // set speed 
                 if (float.TryParse(speed, out var s))
@@ -83,26 +83,68 @@ namespace Disco_Elysium_Mod
                         RunSpeed.runSpeed = 1.0f;
                     }
                 }
+            }
 
+            GUILayout.Label("\nMoney\n[0 - 999]");
+            money = GUILayout.TextField(money, GUILayout.Width(100f));
+            if (GUILayout.Button("Apply", GUILayout.Width(100f)))
+            {
                 // set money
                 if (int.TryParse(money, out var m))
-                {
-                    m *= 100;
-                    if (m >= 0 && m <= 99)
+                { 
+                    if (m >= 0 && m <= 999)
                     {
+                        m *= 100;
                         int currentBalance = LiteSingleton<Sunshine.Metric.PlayerCharacter>.Singleton.Money;
                         LiteSingleton<Sunshine.Metric.PlayerCharacter>.Singleton.Money = m;
                         NotificationSystem.NotificationUtil.ShowMoney(m - currentBalance);
                     }
 
                 }
+            }
 
-                ClothesChange.on = !fixClothes;
+            GUILayout.Label("\nSkillPoints\n[0 - 100]");
+            skillPoints = GUILayout.TextField(skillPoints, GUILayout.Width(100f));
+            if (GUILayout.Button("Apply", GUILayout.Width(100f)))
+            {
+                // set skill points
+                if (int.TryParse(skillPoints, out var p))
+                {
+                    if (p >= 0 && p <= 100)
+                    {
+                        LiteSingleton<Sunshine.Metric.PlayerCharacter>.Singleton.SkillPoints = p;
+                    }
 
+                }
+            }
+
+            GUILayout.Label("\n");
+
+            if (GUILayout.Button("Add All Clothes", GUILayout.Width(400f)))
+            {
+                tester.AddAllClothes();
+            }
+
+            //GUILayout.Label("\n");
+
+            if (GUILayout.Button("Add All Thoughts", GUILayout.Width(400f)))
+            {
+                tester.AddAllThoughts();
+            }
+
+            GUILayout.Label("\n");
+
+            if (GUILayout.Button("Changing Clothes Doesn't Change Appearance: " + (ClothesChange.on ? "OFF" : "ON"), GUILayout.Width(400f)))
+            {
+                ClothesChange.on = !ClothesChange.on;
+            }
+
+            if (GUILayout.Button("Toggle HUD", GUILayout.Width(400f)))
+            {
+                Sunshine.Views.HudToggle.Singleton.ToggleVisibility();
             }
         }
     }
-
 
 
     [HarmonyPatch(typeof(Animator))]
@@ -142,7 +184,7 @@ namespace Disco_Elysium_Mod
         [HarmonyPatch(new Type[] { typeof(string), typeof(bool) })]
         static bool Prefix1()
         {
-            if(ClothesChange.ready)
+            if (ClothesChange.ready)
             {
                 return ClothesChange.on;
             }
@@ -166,5 +208,4 @@ namespace Disco_Elysium_Mod
             }
         }
     }
-
 }
